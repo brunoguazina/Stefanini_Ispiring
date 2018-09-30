a5ef34c0bc449dcf7d6ef772fd751bfcc7647679
brunoguazina <bruno@guazina.com>
2018-09-30 18:29:48 -0300

    updating page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,15 @@
 
 $(document).ready(function(){
 
-	$('.menu').on('click touchstart', function(e){
+	$('.menu').on('touchstart', function(e){
 		$('html').toggleClass('menu-active');
 	  	e.preventDefault();
 	});
 	
+	$('#itens ul li').on('touchstart', function(){
+		$(this).parent().removeClass('active');
+		$(this).toggleClass('active');
+		console.log($(this).parent().parent());
+	});
+
 })
